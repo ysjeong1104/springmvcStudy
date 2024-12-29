0d5de3b4d457cf4a43c8c675e47e69d0e6833d85
ysjeong1104 <whseok@icloud.com>
2024-12-29 21:46:17 +0900

    Initial commit

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,24 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+
+    @GetMapping("log-test")
+    public String logTest() {
+
+        String name = "Spring";
+
+        System.out.println("name = " + name);
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info("info log={}", name);
+        log.warn("warn log={}", name);
+        log.error("error log={}", name);
+        return "ok";
+    }
+}
